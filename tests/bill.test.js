2de89707ebfa37d314d6d4e3924f6e27afd8af1e
Matt Chalmers <matt@barefootnomads.com>
2019-06-27 21:40:32 +1000

    Expand bill testing

--- tests/bill.test.js
+++ tests/bill.test.js
@@ -1,7 +1,7 @@
 
-
-import {BillItem} from "../src/Bill";
+import {BillItem, Bill} from "../src/Bill";
 import {Discount} from "../src/Pricing";
+
 
 describe("Bill Tests", function() {
 
@@ -18,8 +18,34 @@ describe("Bill Tests", function() {
             item.setDiscount('TEST', 'TEST', 100.123);
             expect(item.total()).toEqual(23);
         });
+
+        test("Missing Pricing", function () {
+            let item = new BillItem('standard');
+            expect(() => item.total()).toThrow();
+        });
+
     });
 
+    describe("Bill Tests", function() {
+        test("Non-discounted total", function () {
+            let bill = new Bill(['standard', 'standard']);
+            bill.items[0].price = 123.123;
+            bill.items[1].price = 123.123;
+            expect(bill.total()).toEqual(246.246);
+        });
 
+        test("Discounted Total", function () {
+            let bill = new Bill(['standard', 'standard']);
+            bill.items[0].price = 123.123;
+            bill.items[0].setDiscount('TEST', 'TEST', 100.123);
+            bill.items[1].price = 123.123;
+            expect(bill.total()).toEqual(146.123);
+        });
+
+        test("Missing Pricing", function () {
+            let bill = new Bill(['standard', 'standard']);
+            expect(() => item.total()).toThrow();
+        });
+    });
 
 });
